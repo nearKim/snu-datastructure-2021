c6c73403ba82b24a3abc0b8a76524c9eaa94beb2
nearKim <garfield@toss.im>
2021-10-12 20:20:15 +0900

    initial commit

---
+++ BSTskel.java
@@ -0,0 +1,25 @@
+// (Nearly) Optimal Binary Search Tree
+// Bongki Moon (bkmoon@snu.ac.kr)
+
+public class BST { // Binary Search Tree implementation
+
+  protected boolean NOBSTified = false;
+  protected boolean OBSTified = false;
+
+  public BST() { }
+
+  public int size() { }
+  public void insert(String key) { }
+  public boolean find(String key) { }
+
+  public int sumFreq() { }
+  public int sumProbes() { }
+  public int sumWeightedPath() { }
+  public void resetCounters() { }
+
+  public void nobst() { }	// Set NOBSTified to true.
+  public void obst() { }	// Set OBSTified to true.
+  public void print() { }
+
+}
+
