21218414172e57d54174d30aa3f39845ae3cdf3d
nearKim <garfield@toss.im>
2021-10-16 12:53:55 +0900

    Node 기본구현 추가

--- BST.java
+++ BST.java
@@ -21,5 +21,45 @@ public class BST { // Binary Search Tree implementation
   public void obst() { }	// Set OBSTified to true.
   public void print() { }
 
+  public static class Node {
+    protected int frequency = 1;
+    protected int accessCnt = 0;
+    public String value;
+    public Node left;
+    public Node right;
+
+    public Node(String value) {
+      this.value = value;
+    }
+
+    public Node(String value, Node left, Node right) {
+      this.value = value;
+      this.left = left;
+      this.right = right;
+    }
+
+    public void setLeft(Node left) {
+      this.left = left;
+    }
+    public void setRight(Node right) {
+      this.right = right;
+    }
+
+    public Node getNext(String val) {
+      this.accessCnt += 1;
+      if (this.value.equals(val)) {
+        return this;
+      } else if (this.value.compareTo(val) > 0) {
+        return this.left;
+      } else {
+        return this.right;
+      }
+    }
+
+    public boolean isLeaf() {
+      return (this.left == null) && (this.right == null);
+    }
+
+  }
 }
 
