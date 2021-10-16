f5727225dcd73f53715aea0cca28cf59f903ed5d
nearkim <garfield@snu.ac.kr>
2021-10-16 15:59:29 +0900

    cache를 이용하여 size, insert, find 추가

--- BST.java
+++ BST.java
@@ -1,16 +1,63 @@
 // (Nearly) Optimal Binary Search Tree
 // Bongki Moon (bkmoon@snu.ac.kr)
 
-public class BST { // Binary Search Tree implementation
+import java.util.HashMap;
+import java.util.Objects;
 
+public class BST { // Binary Search Tree implementation
+  private final HashMap<String, Node> nodesCache = new HashMap<>();
   protected boolean NOBSTified = false;
   protected boolean OBSTified = false;
+  public Node root;
 
-  public BST() { }
+  public BST() {  }
 
-  public int size() { }
-  public void insert(String key) { }
-  public boolean find(String key) { }
+  public int size() {
+    return nodesCache.size();
+  }
+
+  private Node insertNode(String key) {
+    // 실제로 Key가 없을 경우 Node를 삽입하고 삽입된 Node를 리턴한다
+    if (root == null) {
+      root = new Node(key);
+      return root;
+    }
+    Node node = root;
+    while (!node.isLeaf()) {
+      node = node.getNext(key);
+    }
+    node = node.insertChild(key);
+    return node;
+  }
+
+  public void insert(String key) {
+    if (nodesCache.containsKey(key)){
+      Node node = nodesCache.get(key);
+      node.addFreq();
+    } else {
+      Node node = insertNode(key);
+      nodesCache.put(key, node);
+    }
+  }
+
+  private Node findNode(String key) {
+    if (root == null) {
+      return null;
+    }
+    Node result = root;
+
+    while (!Objects.equals(result.value, key)) {
+      result = result.getNext(key);
+      if (result == null) {
+        break;
+      }
+    }
+    return result;
+  }
+
+  public boolean find(String key) {
+    return this.findNode(key) != null;
+  }
 
   public int sumFreq() { }
   public int sumProbes() { }
@@ -38,11 +85,22 @@ public class BST { // Binary Search Tree implementation
       this.right = right;
     }
 
-    public void setLeft(Node left) {
-      this.left = left;
+    public Node insertChild(String value) {
+      Node node = new Node(value);
+      int comparator = this.value.compareTo(value);
+
+      if (comparator > 0) {
+          this.left = node;
+      } else if (comparator < 0) {
+        this.right = node;
+      } else {
+        System.out.println("Freq를 올려야 합니다!");
+      }
+      return node;
     }
-    public void setRight(Node right) {
-      this.right = right;
+
+    public void addFreq() {
+      this.frequency +=1;
     }
 
     public Node getNext(String val) {
